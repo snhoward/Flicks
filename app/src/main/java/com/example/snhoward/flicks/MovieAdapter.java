d1d4080e07ee33e7ec798720785c2b63f4d66a6f
Sean Howard <snhoward@fb.com>
2017-06-22 13:26:51 -0700

    modified MovieAdapter.ViewHolder

--- app/src/main/java/com/example/snhoward/flicks/MovieAdapter.java
+++ app/src/main/java/com/example/snhoward/flicks/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.snhoward.flicks;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +13,9 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.example.snhoward.flicks.models.Config;
 import com.example.snhoward.flicks.models.Movie;
+import com.example.snhoward.flicks.models.MovieDetailsActivity;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
@@ -97,8 +101,10 @@ public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.ViewHolder>
         return movies.size();
     }
 
-    // create the viewholder as a static inner class
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    // create the viewholder as an inner class
+    // class *cannot* be static
+    // implements View.OnClickListener
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         // track view objects
         ImageView ivPosterImage;
@@ -113,6 +119,25 @@ public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.ViewHolder>
             ivBackdropImage = (ImageView) itemView.findViewById(R.id.ivBackdropImage);
             tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
             tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            itemView.setOnClickListener(this);
+        }
+
+        // when the user clicks on a row, show MovieDetailsActivity for the selected movie
+        @Override
+        public void onClick(View v) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the movie at the position, this won't work if the class is static
+                Movie movie = movies.get(position);
+                // create intent for the new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                // serialize the movie using parceler, use its short name as a key
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                // show the activity
+                context.startActivity(intent);
+            }
         }
     }
 
